8c6d72bd151bbd0f659afc8e8abf8f5525677da6
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-08-05 20:12:48 +0530

    remove nav bar
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-let nav = document.querySelector(".navigation");
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      nav.classList.add('none');
-    } else {
-        nav.classList.remove('none');
-    }
-  }
+// let nav = document.querySelector(".navigation");
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       nav.classList.add('none');
+//     } else {
+//         nav.classList.remove('none');
+//     }
+//   }
   
-  var x = window.matchMedia("(max-width: 800px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes
+//   var x = window.matchMedia("(max-width: 800px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
 
 
 
-function hideNav() {
-    console.log(nav);
-    nav.classList.toggle("none");
-}
+// function hideNav() {
+//     console.log(nav);
+//     nav.classList.toggle("none");
+// }
